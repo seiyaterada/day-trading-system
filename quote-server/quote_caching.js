51c36ae0c58627a11aeb1a4418eb45388f609ac3
cliu54 <chenghaoliu@uvic.ca>
2023-03-08 20:58:10 -0800

    fixed clientClosedError now both quote puller and caching running as expected

--- quote-server/quote_caching.js
+++ quote-server/quote_caching.js
@@ -1,18 +1,21 @@
 const redis = require('redis');
-const REDIS_PORT = 6379;
-const client_redis = redis.createClient(REDIS_PORT);
+const client_redis =  redis.createClient({
+    legacyMode:true,
+    PORT:6379
+});
 
 function SetRedisData(SYM,data){
-    console.log("Jumping into redis\n");
 
-    try {
-        // key, expiration, data from quote server
-        console.log("processing caching data");
-        client_redis.SETEX(SYM,3600,data)
-        console.log("Complete caching data");
-
-    } catch (err) {
-        console.error(err);
-    }
+    client_redis.connect().then(async() =>{
+        console.log("Jumping into redis caching\n");
+        try {
+            // key, expiration, data from quote server
+            console.log("processing caching data");
+            client_redis.SETEX(SYM,3600,data)
+            console.log("Complete caching data");
+        } catch (err) {
+            console.error(err);
+        }
+    })
 }
 module.exports = {SetRedisData};
--- quote-server/quote_puller.js
+++ quote-server/quote_puller.js
@@ -1,43 +1,31 @@
 const lib = require('./quote_caching');
 const net = require('net');
 
+// Test Data before transaction server online
 const SYM = "S";
 const user_id = "oY01WVirLr";
-
 
 const HOST = 'quoteserve.seng.uvic.ca';
 const QUOTE_PORT = 4444;
 
 // exports.GetQuoteInfo = (sym, userID) =>{
 
-    const client = net.createConnection(QUOTE_PORT, HOST,()=>{
+     const client =  net.createConnection(QUOTE_PORT, HOST,()=>{
         console.log(`Connecting to the quote server with port ${QUOTE_PORT}`);
         client.write(`${SYM} ${user_id} \n`);
-        // client.write(`${SYM} ${user_id} \n`);
     });
 
-    client.on('data',(data)=>{
+     client.on('data',(data)=>{
+        //quote, SYM, user_id, timestamp, cryptokey}
         console.log(`Received: ${data}`);
 
-        //quote, SYM, user_id, timestamp, cryptokey}
-        // var elements =data.split(',');
-        // pass the cryptokey to redis
+        // pass the cryptokey and orginal data to redis
         console.log(`KEY IS: ${SYM}`);
-
         lib.SetRedisData(SYM,data);
         client.destroy();
-        console.log('after destory');
-
     });
 
-    setTimeout(() => {
-        console.log('wait caching settle down');
-    }, 4000);
-
-
-    client.on('close',()=>{
-        console.log('before Closed');
-
+     client.on('close',()=>{
         console.log(`Connection Closed`);
     });
 // }
